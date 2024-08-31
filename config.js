fa2b2aa14c12ec5a447cf184741410e1d9b7adec
ShivamVatsAs <shivamchy867@gmail.com>
2024-08-31 16:38:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Twenty years woven, like threads in a tapestry, Our love's design, intricate and true.",
+        "Through laughter and tears, our bond's artistry, A masterpiece, cherished, me and you. ",
+
+        "Time's river flows, yet our love stands strong, Deeper than roots, reaching for the sky.",
+        "A symphony of trust, where we belong, Two souls entwined, forever, you and I.💖",
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2004-10-11T00:00:00",
+
+
+    names: ["Akanksha🌻", "Shivam🐝"]
+}
